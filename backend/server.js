30359b6e9ef7621c562b2a7651bd630e34c3ad48
desivar <jillian_desire@yahoo.es>
2026-02-06 20:13:45 -0600

    added two line sat the end of server

--- backend/server.js
+++ backend/server.js
@@ -21,3 +21,7 @@ mongoose.connect(process.env.MONGO_URI)
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+const archiveSchema = new mongoose.Schema({ title: String, description: String, date: String, category: String });
+
+const ArchiveItem = mongoose.model('ArchiveItem', archiveSchema);
