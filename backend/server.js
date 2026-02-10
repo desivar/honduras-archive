55da8ac69352dfcb44ea72e7f2323deb601dada1
desivar <jillian_desire@yahoo.es>
2026-02-09 19:01:06 -0600

    cahnge port to 5500

--- backend/server.js
+++ backend/server.js
@@ -19,7 +19,7 @@ mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log("Connected to MongoDB Atlas"))
   .catch(err => console.log(err));
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5500;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
 const archiveSchema = new mongoose.Schema({ title: String, description: String, date: String, category: String });
